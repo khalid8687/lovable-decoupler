06a68f6e5b678661f22474bc385057ae146bad90
khalid8687 <aboganna8687@gmail.com>
2026-07-14 00:27:10 +0300

    chore: add scripts/decouple.js for GitHub Actions build step

--- scripts/decouple.js
+++ scripts/decouple.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const projectDir = process.argv[2];
 if (!projectDir) {
